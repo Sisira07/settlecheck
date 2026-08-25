717fd2d4f637f8dba4b823d09c6262e00385d865
bcs2024070 <asapu.sisira@gmail.com>
2026-08-26 00:11:56 +0530

    Set up Maven Spring Boot project

---
+++ src/main/java/com/settlecheck/SettleCheckApplication.java
@@ -0,0 +1,11 @@
+package com.settlecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleCheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SettleCheckApplication.class, args);
+    }
+}
